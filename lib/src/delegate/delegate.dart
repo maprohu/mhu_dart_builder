7c8c10a4f7d55fb699bdf3a6cd72e036ad7f3c0b
maprohu <maprohu@gmail.com>
2023-08-05 11:25:41 +0100

    publsihing

--- lib/src/delegate/delegate.dart
+++ lib/src/delegate/delegate.dart
@@ -1,6 +1,7 @@
 import 'package:analyzer/dart/element/element.dart';
 import 'package:analyzer/dart/element/nullability_suffix.dart';
 import 'package:analyzer/dart/element/type.dart';
+// ignore: implementation_imports
 import 'package:analyzer/src/dart/constant/value.dart';
 import 'package:build/build.dart';
 import 'package:collection/collection.dart';
